29f0e3312e695371a2746155f40b7981a9a71f3e
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-11-16 00:26:28 +0300

    feat: создать react приложение

---
+++ src/assets/index.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+
+import company from "./company/company.svg";
+
+import reutov from "./reutov.png";
+import projpeter from "./projpeter.png";
+import fingramota from "./fingramota.png";
+
+export {
+  logo,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  company,
+  reutov,
+  projpeter,
+  fingramota
+};
